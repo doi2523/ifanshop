5b0ec65c79ad6a821bb86ec92d50eb4422d277a8
doibncm2003 <doibncm2003@gmail.com>
2024-03-27 11:09:57 +0700

    up3

--- js/script.js
+++ js/script.js
@@ -34,3 +34,34 @@ $(window).on("load",function (){
 	topOffset: -63
 	});
 });
+
+// $(document).ready(function(){
+//     $('.nav-item.dropdown').hover(function(){
+//         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+//     }, function(){
+//         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+//     });
+// });
+$(document).ready(function(){
+    $('.nav-item.dropdown').hover(function(){
+        $(this).find('.dropdown-menu').stop(true, true).fadeIn(500);
+    }, function(){
+        $(this).find('.dropdown-menu').stop(true, true).fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+    $('.nav-link.dropdown-toggle').hover(function(){
+        $(this).find('.rotate-icon').addClass('hovered');
+    }, function(){
+        $(this).find('.rotate-icon').removeClass('hovered');
+    });
+});
+
+
+
+
+
+
+
+
