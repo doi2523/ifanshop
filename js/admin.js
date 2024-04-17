ab00975753a7ceedf182c6fc0740808239a10af1
doibncm2003 <doibncm2003@gmail.com>
2024-04-17 19:02:36 +0700

    hhhh

--- js/admin.js
+++ js/admin.js
@@ -46,22 +46,29 @@ menuicn.addEventListener("click", () => {
 })
 
 
-$(document).ready(() => {
-    $("#open-sidebar").click(() => {
-      // add class active on #sidebar
-      $("#sidebar").addClass("active");
+$(document).ready(()=>{
   
-      // show sidebar overlay
-      $("#sidebar-overlay").removeClass("d-none");
-    });
-  
-    $("#sidebar-overlay").click(function () {
-      // add class active on #sidebar
-      $("#sidebar").removeClass("active");
-  
-      // show sidebar overlay
-      $(this).addClass("d-none");
-    });
+    $('#open-sidebar').click(()=>{
+       
+        // add class active on #sidebar
+        $('#sidebar').addClass('active');
+        
+        // show sidebar overlay
+        $('#sidebar-overlay').removeClass('d-none');
+      
+     });
+    
+    
+     $('#sidebar-overlay').click(function(){
+       
+        // add class active on #sidebar
+        $('#sidebar').removeClass('active');
+        
+        // show sidebar overlay
+        $(this).addClass('d-none');
+      
+     });
+    
   });
   
   
