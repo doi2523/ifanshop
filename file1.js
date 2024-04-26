767e8a1dbfc21e7f18d6e51e371a42492c4eaf94
doibncm2003 <doibncm2003@gmail.com>
2024-04-26 22:57:41 +0700

    qlysanpham

---
+++ file1.js
@@ -0,0 +1,4 @@
+// Định nghĩa một hàm trong file1.js
+function functionInFile1() {
+    console.log("This function is from File1.js");
+}
---
+++ file2.js
@@ -0,0 +1,6 @@
+// Gọi hàm từ file1.js trong file2.js
+function functionInFile2() {
+    // Gọi hàm từ file1.js
+    functionInFile1();
+    console.log("This function is from File2.js");
+}
